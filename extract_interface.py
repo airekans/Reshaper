2fb40a58076acf8eca295c603e420acabe8bbbe1
Yaolong Huang <airekans@gmail.com>
2013-02-07 17:01:29 +0800

    Finish a test version of extract interface

---
+++ extract_interface.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python
+
+from clang.cindex import CursorKind
+from clang.cindex import TranslationUnit
+from clang.cindex import TypeKind
+from clang.cindex import Cursor
+from clang.cindex import Type
+import os
+import sys
+from util import get_cursor
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print "Please input .cpp file and class."
+        sys.exit(1)
+    
+    source_dir = os.path.join(os.path.dirname(__file__), 'test_data')
+    src = os.path.join(source_dir, sys.argv[1])
+    class_to_extract = sys.argv[2]
+
+    tu = TranslationUnit.from_source(src)
+    class_cursor = get_cursor(tu, class_to_extract)
+    
+    if class_cursor is None or \
+            class_cursor.kind != CursorKind.CLASS_DECL or \
+            not class_cursor.is_definition():
+        print "source file %s does not contain any class" % src
+        sys.exit(1)
+
+    member_cursors = list(class_cursor.get_children())
+
+    member_method_cursors = [m for m in member_cursors
+                             if m.kind == CursorKind.CXX_METHOD]
+
+    for m in member_method_cursors:
+        print "name of the member:", m.spelling
+        print "kind:", m.kind.name
+        print "type name:", m.type.kind.spelling
+
+        arg_types = m.type.argument_types()
+        result_type = m.type.get_result()
+
+        print "  result type:", result_type.kind.name
+
+        for arg in arg_types:
+            print " arg type:", arg.kind.name
+        
+        print
+    
+
+
