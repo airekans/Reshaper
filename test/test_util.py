369f26ae79fc6b23df8579bad2576f384a208ee8
liangzhao <liangzhao@liangzhao-VirtualBox.(none)>
2013-03-07 15:13:10 +0800

    ddd

--- test/test_util.py
+++ test/test_util.py
@@ -9,7 +9,8 @@ import os, pickle
 def compare(obj, golden_file_path):
     '''
     if the golden file dose not exist, will generate it and return TRUE,
-    otherwise will return False and error message(difference of two obj) 
+    otherwise it will compare the obj with the golden file.
+    When return False , error message will contain difference of two obj 
     '''
     if not os.path.isfile(golden_file_path):
           golden_file = open(golden_file_path,'w')
