54838d0bc3cb22d4262d56eeab509b2941b24b72
liangzhao <liangzhao@liangzhao-vm-centos.(none)>
2013-04-11 19:26:11 +0800

    add option to ast_dump

--- ast_dump.py
+++ ast_dump.py
@@ -3,7 +3,7 @@
 from reshaper.util import get_tu, walk_ast
 from clang.cindex import TranslationUnit
 from optparse import OptionParser
-import sys
+import sys, os
 
 def print_cursor(cursor, level):
     prefix = "**" * level
@@ -13,7 +13,7 @@ def print_cursor(cursor, level):
     print prefix + "spelling:", cursor.spelling
     print prefix + "displayname:", cursor.displayname
     print prefix + "kind:", cursor.kind.name
-
+    
     if cursor.type is not None:
         print prefix + "type kind:", cursor.type.kind.name
     
@@ -25,16 +25,43 @@ def print_cursor(cursor, level):
     print 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print 'invalid number arguments'
+    
+    option_parser = OptionParser(usage = "%prog [options] files") 
+    option_parser.add_option("-l", "--level", dest = "level",
+                             type="int",\
+                             help = "max level to print")
+    option_parser.add_option("-a", "--all", dest = "all",
+                             action="store_true",
+                             help = "walk all cursor nodes including\
+                                     the ones not defined in this file")
+    
+    (options, args) = option_parser.parse_args()
+    
+    if len(args) < 1:
+        print 'Please input files to parse'
         sys.exit(1)
+          
+    
+    FILE_PATHS = set([os.path.abspath(path) for path in args])
 
-    tu = get_tu(sys.argv[1])
-    if not tu:
-        print "unable to load input"
-        sys.exit(1)
-
-    walk_ast(tu, print_cursor)
+    def can_visit_cursor_func(cursor, level):
+        can_visit =  True
+        if options.level is not None:
+            can_visit = (level <= options.level)
+        if not options.all :
+            cursor_file = cursor.location.file
+            if cursor_file:
+                can_visit = can_visit and \
+                                os.path.abspath(cursor_file.name) in FILE_PATHS
+        return can_visit
+        
+    for file_path in args:     
+        tu = get_tu(file_path)
+        if not tu:
+            print "unable to load %s" % file_path
+            sys.exit(1)
+    
+        walk_ast(tu, print_cursor, can_visit_cursor_func)
     
 
     
