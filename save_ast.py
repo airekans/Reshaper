8826d6bebb63ac51e7197dc806fe9744d242fb8a
liangzhao <liangzhao@liangzhao-vm-centos.(none)>
2013-05-13 09:08:17 +0800

    add file

---
+++ save_ast.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from reshaper.ast import save_ast
+from optparse import OptionParser
+import sys
+import os
+
+
+def main():
+   
+    option_parser = OptionParser(usage = "%prog [options] files") 
+    option_parser.add_option("-d", "--dir", dest = "dir", \
+                             type="string", default='', \
+                             help = "max level to print")
+    option_parser.add_option("-r", "--readable", dest = "readable", \
+                             action="store_true", \
+                             help = "dump with readable format (slow)")
+    (options, args) = option_parser.parse_args()
+       
+    if len(args) < 1:
+        option_parser.error('Please input files to parse')
+    
+    for file_path in args:     
+        save_ast(file_path, options.dir, options.readable)
+        
+        
+if __name__ == '__main__':
+    main()
+    
