2d035ba4c7bbfbf3f5ff8350b4bd3d476fe99ebc
liangzhao <liangzhao@liangzhao-vm-centos.(none)>
2013-05-30 16:39:25 +0800

    script done

--- gen_collaboration_graph.py
+++ gen_collaboration_graph.py
@@ -5,7 +5,9 @@ Created on May 30, 2013
 '''
 from optparse import OptionParser
 import os
-
+from reshaper.option import setup_options
+from reshaper.ast import get_tu
+from reshaper.dot_gen_util import gen_class_collaboration_graph
 
 def main():
     ''' main '''
@@ -14,17 +16,26 @@ def main():
     option_parser.add_option("-f", "--file", dest = "file", \
                              type="string", default='', \
                              help = "input file path ")
-    
-    
+    setup_options(option_parser)
     (options, args) = option_parser.parse_args()
     
     file_path = options.file
+    
+    if not file_path:
+        option_parser.error('-f option is not set')
+        
     if not os.path.isfile(file_path):
         option_parser.error('file %s dose not exist' % file_path)
         
     class_names = args
     if not class_names:
         option_parser.error('Please input class names')
+        
+    tu_source = get_tu(os.path.abspath(file_path),
+                       config_path = options.config,
+                       cdb_path = options.cdb_path)
+    
+    print gen_class_collaboration_graph(tu_source, class_names)
 
 if __name__ == '__main__':
     main()
