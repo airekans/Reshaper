9d952be80e056da0440b29c5e993959e78c90757
Jin-Hua Ouyang <jouyang@bcxcp-AS-dev01.briontech.com>
2013-07-04 15:57:27 +0800

    add class CallInfo FileClassInfo ClassInfo and UT for it. add walk_ast_add_caller, get_declaration_location_usr, get_usr_if_caller and related UT

--- reshaper/find_reference_util.py
+++ reshaper/find_reference_util.py
@@ -1,19 +1,274 @@
 """util functions for finding reference
 """
 import os, sys
-from clang.cindex import CursorKind
+from clang.cindex import CursorKind, TypeKind
 from reshaper.util import  check_diagnostics
 from reshaper.ast import get_tu
-from reshaper.util import get_declaration
+from reshaper.util import walk_ast, get_cursors_if, get_declaration
 from reshaper.semantic import get_cursors_add_parent, is_cursor
 from reshaper.option import setup_find_reference_options
 from optparse import OptionParser
+from functools import partial
 import reshaper.option
+
+_class_mem_cursorkind = ("CONSTRUCTOR", "DESTRUCTOR", "CXX_METHOD", "FIELD_DECL")
+
+class class_info(object):
+    """get class info include member and functions
+    from class cursor 
+    """
+    def __init__(self, cursor):
+        """ should make sure that cursor.kind is one of 
+        CLASS_DECL or STRUCT_DECL
+        """
+        if cursor is None or cursor.kind is None:
+            return
+
+        if not cursor.kind == CursorKind.CLASS_DECL:
+            return
+
+        self._cursor = cursor
+        self._class_name = cursor.displayname
+        self._mem_fun_dict = {}
+        self._mem_dict = {}
+        self._update_mem_dicts()
+
+    def _update_mem_dicts(self):
+        """internal function, used to parse class cursor 
+        and get infors
+        """
+        def walk_ast_cursor_fun(cursor, level, mem_dict, mem_fun_dict):
+            """function used in walk_ast
+            """
+            if cursor.kind and cursor.kind.name in _class_mem_cursorkind:
+                if cursor.kind == CursorKind.FIELD_DECL:
+
+                    def get_mem_tokens(cursor):
+                        """get string of tokens from cursor for mem
+                        """
+                        if not cursor or not cursor.kind or \
+                                not cursor.kind == CursorKind.FIELD_DECL:
+                                    return None
+
+                        tokens_str = ""
+                        for token in cursor.get_tokens():
+                            if token.spelling == cursor.displayname:
+                                tokens_str += " " + token.spelling
+                            else:
+                                tokens_str += token.spelling
+                        return tokens_str
+
+                    mem_dict[cursor.get_usr()] = get_mem_tokens(cursor)
+                else:
+                    mem_fun_dict[cursor.get_usr()] = cursor.displayname
+        walk_ast(self._cursor, partial(walk_ast_cursor_fun,\
+                mem_dict = self._mem_dict, mem_fun_dict = self._mem_fun_dict))
+
+
+    def get_class_name(self):
+        """return paser class name
+        """
+        return self._class_name
+
+    def get_mem_list(self):
+        """return mem list of class
+        """
+        return self._mem_dict.values()
+
+    def get_mem_usr_list(self):
+        """return usr list of mems in class
+        """
+        return self._mem_dict.keys()
+
+    def get_mem_func_list(self):
+        """return func list of class
+        """
+        return self._mem_fun_dict.values()
+
+    def get_mem_func_usr_list(self):
+        """return usrs of funcs in class
+        """
+        return self._mem_fun_dict.keys()
+
+    def get_mem_with_usr_dict(self):
+        """return dict of 
+        {mem_usr, mem_tokens}
+        """
+        return self._mem_dict
+
+    def get_mem_func_with_ust_dict(self):
+        """return dict of 
+        {func_usr, func_displayname}
+        """
+        return self._mem_fun_dict
+
+
+class classes_info(object):
+    """parse file or tu to get class_info object
+    for all classes and structs
+    """
+    def __init__(self, filename, cdb_path = None, is_tu = False, tu = None):
+        """1) should make sure that filename exists 
+           2) you can also pass tu object to it
+        """
+        if tu is not None:
+            self._file_tu = tu
+        else:
+            self._file_tu = get_tu(os.path.abspath(filename),\
+                    cdb_path = cdb_path)
+            if self._file_tu is None:
+                return
+
+        class_cursors = self._get_class_cursors()
+        self._class_to_info_dict = {}
+        for cursor in class_cursors:
+            self._class_to_info_dict[cursor.displayname]  = \
+                    class_info(cursor)
+
+    def _get_class_cursors(self):
+        """get class and struct cursors in tu
+        """
+        def is_class_cursor(cursor):
+            if cursor and cursor.kind:
+                if cursor.kind == CursorKind.CLASS_DECL\
+                        or cursor.kind == CursorKind.STRUCT_DECL:
+                    return True
+            return False
+
+        class_cursors = get_cursors_if(self._file_tu, is_class_cursor)
+        return class_cursors
+
+    def get_class_with_inf_dict(self):
+        """return classes info format with
+        {class_name, class_info}
+        """
+        return self._class_to_info_dict
+
+    def get_class_list(self):
+        """return class name list
+        """
+        return self._class_to_info_dict.keys()
+
+    def get_class_info_list(self):
+        """return class_info list
+        """
+        return self._class_to_info_dict.values()
+
+    def get_class_info_with_name(self, name):
+        """get class info with name
+        """
+        return self._class_to_info_dict[name]
+        
+class call_info(object):
+    """serialize and deserialize for 
+    caller and caller str.
+       Format is as follows:
+       caller:value1,value2,...,:callee:value1,...
+    value can be any kind of str.(location and usr)
+
+    caller_callee_split is the split between caller and
+    callee, inter_split is the split between callers
+    and callees, key_value_split is the split between 
+    caller(callee) and value
+    """
+    def __init__(self, input_str,\
+            caller_callee_split = ";", inter_split = ",",\
+            key_value_split = "|"):
+        """if input_str is not None, should make sure
+        it in the format of "caller..;caller"
+        """
+        self._caller_callee_split = caller_callee_split
+        self._inter_split = inter_split
+        self._key_value_split = key_value_split
+
+        if input_str is None or input_str == "":
+            self._callers_str = ""
+            self._callees_str = ""
+            return 
+
+        split_str = input_str.split(caller_callee_split)
+        assert(len(split_str) == 2)
+
+        self._callers_str = \
+                split_str[0].split(key_value_split)[1]
+        self._callees_str = \
+                split_str[1].split(key_value_split)[1]
+
+    def get_caller(self):
+        """return callers_str in the format of
+        value1,value2
+        """
+        return self._callers_str
+
+    def get_caller_list(self):
+        """return callers in the format of 
+        [value1, value2]
+        """
+        return self._callers_str.split(self._inter_split)
+
+    def get_callee(self):
+        """return callees_str in the format of 
+        value1,value2
+        """
+        return self._callees_str
+
+    def get_callee_list(self):
+        """return callee str in the format of
+        [value1, value]
+        """
+        return self._callees_str.split(self._inter_split)
+
+    def add_caller(self, caller_str):
+        """add caller_str
+        """
+        should_add = False
+        if caller_str is not None:
+            if self._callers_str == "":
+                should_add = True
+            elif not caller_str in self._callers_str:
+                should_add = True
+
+        if should_add:
+            if self._callers_str == "":
+                self._callers_str = "%s" % caller_str
+            else:
+                self._callers_str = "%s%s%s" % \
+                        (self._callers_str, self._inter_split, caller_str)
+
+    def add_callee(self, callee_str):
+        """add callee str
+        """
+        should_add = False
+        if callee_str is not None:
+            if self._callees_str == "":
+                should_add = True
+            elif not callee_str in self._callees_str:
+                should_add = True
+
+        if should_add:
+            if self._callees_str == "":
+                self._callees_str = callee_str
+            else:
+                self._callees_str = "%s%s%s" \
+                    % (self._callees_str, self._inter_split, callee_str)
+
+    def clear(self):
+        self._callees_str = ""
+        self._callers_str = ""
+
+    def get_callers_callees_str(self):
+        """return callers and callees str
+        """
+        return "caller%s%s%scallee%s%s" %\
+                (self._key_value_split, self._callers_str, \
+                self._caller_callee_split, self._key_value_split, \
+                self._callees_str)
 
 def get_usr_of_declaration_cursor(cursor):
     """get declaration cursor and return its USR
     """
-    declaration_cursor = cursor.get_declaration()
+    declaration_cursor = get_declaration(cursor)
+    #declaration_cursor = cursor.get_declaration()
     if is_cursor(declaration_cursor):
         return declaration_cursor.get_usr()
     return None
@@ -49,7 +304,7 @@ def get_cursors_with_name(file_name, name, ref_curs):
     if not os.path.exists(file_name):
         print "file %s don't exists\n" % file_name
         return
-    current_tu = get_tu(file_name)
+    current_tu = get_tu(file_name, cdb_path="/home/jouyang/tachyon10/")
     if check_diagnostics(current_tu.diagnostics):
         print "Warning : diagnostics occurs, skip file %s" % file_name
        # return
@@ -97,4 +352,65 @@ def parse_find_reference_args(default_output_filename):
             options.output_file_name = tmp_output_file 
 
     return options
- 
+
+def walk_ast_add_caller(source, visitor, is_visit_subtree_fun = lambda _c : True):
+    """walk ast and add caller info (usr, location)
+    to visitor func
+    """
+    if source is None:
+        return
+    elif hasattr(source, "get_children"):
+        cursor = source
+    else:
+        cursor = source.cursor
+
+    def walk_ast_and_add_caller(cursor, usr, location):
+        if not is_visit_subtree_fun(cursor):
+            return
+
+        visitor(cursor, usr, location)
+        for c in cursor.get_children():
+            caller_usr = usr
+            location_str = location
+            if caller_usr is None:
+                caller_usr = get_usr_if_caller(c)
+                if caller_usr is None:
+                    location_str = None
+                else:
+                    location_str = get_declaration_location_str(c)
+
+            walk_ast_and_add_caller(c, caller_usr, location_str)
+
+    source_usr = get_usr_if_caller(cursor)
+    if source_usr is None:
+        source_location = None
+    else:
+        source_location = get_declaration_location_str(cursor)
+
+    walk_ast_and_add_caller(cursor, source_usr, source_location)
+
+def get_usr_if_caller(cursor):
+    """if cursor is caller, return its usr
+    """
+    if cursor is None or cursor.type is None:
+        return None
+
+    if cursor.is_definition() and cursor.type.kind == TypeKind.FUNCTIONPROTO:
+        return get_usr_of_declaration_cursor(cursor)
+
+def get_declaration_location_str(cursor):
+    """get cursor's declaration str info,
+    return value's format is file.name-line-column
+    """
+    if cursor is None:
+        return None
+    decl_cursor = get_declaration(cursor)
+    if decl_cursor is None or decl_cursor.location is None\
+            or decl_cursor.location.file is None:
+                return None
+
+    location_str = "%s-%s-%s" % \
+           (decl_cursor.location.file.name, decl_cursor.location.line,\
+           decl_cursor.location.column)
+    return location_str
+
--- tests/test_find_reference_util.py
+++ tests/test_find_reference_util.py
@@ -2,11 +2,16 @@
 find_reference_util.py
 '''
 
+from clang.cindex import CursorKind, TranslationUnit
 from nose.tools import eq_
 from reshaper.util import get_cursor_with_location
 from reshaper.semantic import get_cursors_add_parent
-from reshaper.find_reference_util import filter_cursors_by_usr
+from reshaper.find_reference_util import filter_cursors_by_usr, \
+        get_usr_if_caller, class_info, classes_info,\
+        call_info, walk_ast_add_caller,\
+        get_usr_of_declaration_cursor, get_declaration_location_str
 from .util import get_tu_from_text
+from functools import partial
 
 filter_usr_test_input = """\
 class TestClass
@@ -33,6 +38,192 @@ void CallFunc()
 }
 """
 
+class_info_test = """\
+class TestClass
+{
+public:
+    TestClass();
+    TestClass(int x) : m_x(x) {}
+    ~TestClass(){}
+
+    void class_func1();
+    bool class_func2() {}
+
+    static void static_func() {}
+private:
+    int m_x;
+    double m_d;
+    long* m_pL;
+    char* m_ch;
+};
+"""
+
+class_info_from_file_test = """\
+class TestClass1
+{
+};
+
+class TestClass2 : public TestClass1
+{
+public:
+    TestClass2();
+};
+
+struct TestStruct
+{
+int m_int;
+double m_double;
+};
+"""
+
+walk_ast_add_caller_test_file = """\
+void call_func()
+{
+}
+
+namespace {
+    void namespace_func()
+    {
+    }
+}
+
+class TestClass
+{
+public:
+    void class_func()
+    {
+        call_func();
+        m_int = 10;
+    }
+
+public:
+    double m_public;
+
+private:
+    int m_int;
+};
+
+void global_func()
+{
+    ::namespace_func();
+    call_func();
+    TestClass t;
+    t.class_func();
+    t.m_public = 0.0;
+}
+"""
+
+cursor_location_usr_test_file = """\
+void call_func();
+void test_func()
+{
+}
+"""
+
+def test_get_declaration_location_usr():
+    '''test get_declaration_location_str
+    '''
+    text_tu = get_tu_from_text(cursor_location_usr_test_file)
+    cursor1 = get_cursor_with_location(text_tu, "call_func", 1)
+    cursor2 = get_cursor_with_location(text_tu, "test_func", 2)
+    none_cursor = get_cursor_with_location(text_tu, "TestClass", 4)
+
+    location_cursor1 = get_declaration_location_str(cursor1)
+    location_cursor2 = get_declaration_location_str(cursor2)
+    location_none = get_declaration_location_str(none_cursor)
+    eq_("t.cpp-1-6", location_cursor1)
+    eq_("t.cpp-2-6", get_declaration_location_str(cursor2))
+    assert(location_none is None)
+
+
+def test_caller_callee_str_caller():
+    '''test call_info caller
+    '''
+    test_str = call_info("")
+    caller1 = "testfile-14-10"
+    caller2 = "testfile-24-10"
+
+    test_str.add_caller(caller1)
+    eq_(caller1, test_str.get_caller())
+    test_str.add_caller(caller1)
+    eq_(caller1, test_str.get_caller())
+    eq_(len(test_str.get_caller_list()), 1)
+
+    test_str.add_caller(caller2)
+    expect_caller = "%s,%s" % (caller1, caller2)
+    eq_(expect_caller, test_str.get_caller())
+    eq_(len(test_str.get_caller_list()), 2)
+
+    expect_str = "caller|%s;callee|" % expect_caller
+    eq_(expect_str, test_str.get_callers_callees_str())
+
+    test_str.clear()
+    eq_(test_str.get_caller(), "")
+
+def test_caller_callee_str_callee():
+    '''test call_info callee
+    '''
+    test_str = call_info("")
+    callee1 = "testfile-34-10"
+    callee2 = "testfile-44-10"
+
+    test_str.add_callee(callee1)
+    eq_(callee1, test_str.get_callee())
+    test_str.add_callee(callee1)
+    eq_(callee1, test_str.get_callee())
+    eq_(len(test_str.get_callee_list()), 1)
+
+    test_str.add_callee(callee2)
+    expect_callee = "%s,%s" % (callee1, callee2)
+    eq_(expect_callee, test_str.get_callee())
+    eq_(len(test_str.get_callee_list()), 2)
+
+    expect_str = "caller|;callee|%s" % expect_callee
+    eq_(expect_str, test_str.get_callers_callees_str())
+
+    test_str.clear()
+    eq_(test_str.get_callee(), "")
+
+def test_classes_info():
+    '''test classes info from tu
+    '''
+    text_tu = get_tu_from_text(class_info_from_file_test)
+    classes_info_test = classes_info(filename = None, tu = text_tu)
+    class_names = classes_info_test.get_class_list()
+    eq_(len(class_names), 3)
+
+    TestClass1_info = classes_info_test.get_class_info_with_name("TestClass1")
+    TestClass2_info = classes_info_test.get_class_info_with_name("TestClass2")
+    TestStruct_info = classes_info_test.get_class_info_with_name("TestStruct")
+    assert(isinstance(TestClass1_info, class_info))
+    assert(isinstance(TestClass2_info, class_info))
+    assert(isinstance(TestStruct_info , class_info))
+
+
+def test_class_info():
+    '''test class class_info
+    '''
+    text_tu = get_tu_from_text(class_info_test)
+    class_cursor = get_cursor_with_location(text_tu, "TestClass", 1)
+    eq_(class_cursor.kind, CursorKind.CLASS_DECL)
+    testclass_mem_info = class_info(class_cursor)
+
+    mem_list = testclass_mem_info.get_mem_list()
+    eq_(len(mem_list), 4)
+    assert("int m_x" in str(mem_list))
+    assert("double m_d" in str(mem_list))
+    assert("long* m_pL" in str(mem_list))
+    assert("char* m_ch" in str(mem_list))
+
+    func_list = testclass_mem_info.get_mem_func_list()
+    eq_(len(func_list), 6)
+    assert("TestClass" in str(func_list))
+    assert("~TestClass" in str(func_list))
+    assert("class_func1" in str(func_list))
+    assert("class_func2" in str(func_list))
+    assert("static_func" in str(func_list))
+    
+
 def test_filter_cursurs_by_usr():
     '''test function filter_cursors_by_usr
     '''
@@ -48,4 +239,99 @@ def test_filter_cursurs_by_usr():
     eq_(len(final_curs), 2)
     eq_(final_curs[0].location.line, 4)
     eq_(final_curs[1].location.line, 19)
+
+def test_get_usr_if_caller():
+    '''test get_usr_if_caller
+    '''
+    text_tu = get_tu_from_text(filter_usr_test_input)
+    class_decl_cursor = get_cursor_with_location(text_tu, "TestClass", 1)
+    class_decl_test = get_usr_if_caller(class_decl_cursor)
+    assert(class_decl_test is None)
+
+    cxx_method_cursor = get_cursor_with_location(text_tu, "TargetFunc", 4)
+    cxx_method_test = get_usr_if_caller(cxx_method_cursor)
+    assert(cxx_method_test is not None)
+
+    func_defin_cursor = get_cursor_with_location(text_tu, "CallFunc", 16)
+    func_defin_test = get_usr_if_caller(func_defin_cursor)
+    assert(func_defin_test is not None)
+
+    func_ref_cursor = get_cursor_with_location(text_tu, "TargetFunc", 19)
+    func_ref_test = get_usr_if_caller(func_ref_cursor)
+    assert(func_ref_test is None)
+
+def walk_ast_add_caller_visitor(cursor, caller_usr, location, call_info_dict):
+    """used as visitor for walk_ast_add_caller
+    """
+    if cursor is None or not cursor.kind or caller_usr is None:
+        return
+    if cursor.kind == CursorKind.CALL_EXPR or \
+            cursor.kind == CursorKind.UNEXPOSED_EXPR or \
+            cursor.kind == CursorKind.COMPOUND_STMT or \
+            cursor.kind == CursorKind.BINARY_OPERATOR:
+                return
+
+    decl_usr = get_usr_of_declaration_cursor(cursor)
+    if decl_usr is None:
+        return
+
+    if decl_usr == caller_usr:
+        return
+
+    cursor_location = get_declaration_location_str(cursor)
+
+    cursor_info  = ""
+    caller_info = ""
+    if cursor_location in call_info_dict.keys():
+        cursor_info = call_info_dict[cursor_location]
+    if location in call_info_dict.keys():
+        caller_info = call_info_dict[location]
+
+    cursor_info_obj = call_info(cursor_info)
+    caller_info_obj = call_info(caller_info)
+
+    cursor_info_obj.add_caller(caller_usr)
+    caller_info_obj.add_callee(decl_usr)
+
+    call_info_dict[cursor_location] = cursor_info_obj.get_callers_callees_str()
+    call_info_dict[location] = caller_info_obj.get_callers_callees_str()
+
+
+def test_walk_ast_add_caller():
+    """
+    test walk_ast_add_caller:
+
+    cursors with information should be
+     call_func, namespace_func, TestClass
+     , class_func, m_public, m_int, global_func
+     and t defined in global_func (total are 8)
+
+     only global_func and class_func have callee;
+     1) global_func has callee : namespace_func, TestClass, 
+         t, class_func, m_public, call_func
+     2) class_func has callee : call_func, m_int
+     3) only class_func has both caller and callee
+    """
+    call_info_dict = {}
+    text_tu = get_tu_from_text(walk_ast_add_caller_test_file)
+    walk_ast_add_caller(text_tu, \
+            partial(walk_ast_add_caller_visitor, call_info_dict = call_info_dict))
+
+    eq_(8, len(call_info_dict))
+    with_callee_num = 0
+    with_caller_num = 0
+    with_callee_and_caller_num = 0
+    for key in call_info_dict.keys():
+        call_infor = call_info(call_info_dict[key])
+        caller_str = call_infor.get_caller()
+        callee_str = call_infor.get_callee()
+        if not caller_str == "":
+            with_caller_num += 1
+        if not callee_str == "":
+            with_callee_num += 1
+        if not caller_str == "" and not callee_str == "":
+            with_callee_and_caller_num += 1
+    eq_(2, with_callee_num)
+    eq_(7, with_caller_num)
+    eq_(1, with_callee_and_caller_num)
 
