b875c148eef65e5bd094bfdf15ba66cb70bc8c24
yahuang <yao-long.huang@asml.com>
2013-08-29 12:27:40 +0800

    Add with_param_setup.

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,7 +1,7 @@
 from reshaper import db
 from clang.cindex import CursorKind as ckind
 from clang.cindex import TypeKind as tkind
-from nose.tools import eq_, with_setup
+from nose.tools import eq_, with_setup, nottest
 from .util import get_tu_from_text
 from reshaper.ast import get_tu
 import os
@@ -70,12 +70,23 @@ int main()
     eq_(expected_file_names, actual_file_names)
     
 
-_tu = None
-_proj_engine = None
+def with_param_setup(setup):
+    """ util decorator to pass parameters to test functions
+    """
+    
+    def decorate(func):
+        
+        def wrap_func():
+            param_dict = setup()
+            func(**param_dict) # test function returns nothing
+        
+        wrap_func.func_name = func.func_name
+        return wrap_func
+    
+    return decorate
 
+@nottest
 def setup_for_test_file():
-    global _tu, _proj_engine
-
     TEST_INPUT = \
 """
 int main()
@@ -86,25 +97,32 @@ int main()
 
     _tu = get_tu_from_text(TEST_INPUT, 't.cpp')
     _proj_engine = db.ProjectEngine('test', is_in_memory = True)
-    
-@with_setup(setup_for_test_file)
-def test_file_get_pending_filenames():
-    pending_files = db.File.get_pending_filenames(_tu, _proj_engine)
+    return {'tu': _tu, 'proj_engine': _proj_engine}
+
+@with_param_setup(setup_for_test_file)
+def test_file_get_pending_filenames(tu, proj_engine):
+    pending_files = db.File.get_pending_filenames(tu, proj_engine)
     eq_(set(['t.cpp']), pending_files)
 
-    _proj_engine.build_db_file(_tu)
-    pending_files = db.File.get_pending_filenames(_tu, _proj_engine)
+    proj_engine.build_db_file(tu)
+    pending_files = db.File.get_pending_filenames(tu, proj_engine)
     assert not pending_files
 
-@with_setup(setup_for_test_file)
-def test_file_from_clang_tu():
-    expected_file = _tu.get_file(_tu.spelling)
-    _file = db.File.from_clang_tu(_tu, _tu.spelling, _proj_engine)
+@with_param_setup(setup_for_test_file)
+def test_file_from_clang_tu(tu, proj_engine):
+    expected_file = tu.get_file(tu.spelling)
+    _file = db.File.from_clang_tu(tu, tu.spelling, proj_engine)
     eq_(expected_file.name, _file.name)
     eq_(expected_file.time, _file.time)
 
-@with_setup(setup_for_test_file)
-def test_cursor_kind():   
+def test_file_from_clang_tu_with_multiple_files():
+    FILE_TEST_DIR = os.path.join(_TEST_DATA_DIR, 'db', 'file')
+    SOURCE_PATH = os.path.join(FILE_TEST_DIR, 'main.cpp')
+#    _tu = get_tu(SOURCE_PATH, is_from_cache_first = False)
+    
+
+@with_param_setup(setup_for_test_file)
+def test_cursor_kind(tu, proj_engine):   
     def assert_ckind_equal(ckind, db_ckind):
         eq_(ckind.name, db_ckind.name)
         eq_(ckind.is_declaration(), db_ckind.is_declaration)
@@ -117,16 +135,16 @@ def test_cursor_kind():
         eq_(ckind.is_unexposed(), db_ckind.is_unexposed)
     
     for kind in ckind.get_all_kinds():
-        db_ckind = db.CursorKind.from_clang_cursor_kind(kind, _proj_engine)
+        db_ckind = db.CursorKind.from_clang_cursor_kind(kind, proj_engine)
         assert_ckind_equal(kind, db_ckind)
 
-@with_setup(setup_for_test_file)
-def test_type_kind():
+@with_param_setup(setup_for_test_file)
+def test_type_kind(tu, proj_engine):
     def assert_tkind_equal(tkind, db_tkind):
         eq_(tkind.name, db_tkind.name)
         eq_(tkind.spelling, db_tkind.spelling)
     
     for kind in _get_clang_type_kinds():
-        db_tkind = db.TypeKind.from_clang_type_kind(kind, _proj_engine)
+        db_tkind = db.TypeKind.from_clang_type_kind(kind, proj_engine)
         assert_tkind_equal(kind, db_tkind)
 
