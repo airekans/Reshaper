7e2c0657bcaab6266883783cc313f1c48012b7cb
yahuang <yao-long.huang@asml.com>
2013-08-30 12:44:56 +0800

    Add tests for from_clang_cursor.

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,10 +1,14 @@
 from reshaper import db
 from clang.cindex import CursorKind as ckind
 from clang.cindex import TypeKind as tkind
+import clang.cindex as cindex
 from nose.tools import eq_, with_setup, nottest
 from .util import get_tu_from_text
 from reshaper.ast import get_tu
+from reshaper.util import get_cursor
 import os
+from functools import partial
+from _xmlplus.xpath.XPathParser import PARENT
 
 
 _TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), 'test_data')
@@ -85,6 +89,13 @@ def with_param_setup(setup):
     
     return decorate
 
+
+@nottest
+def setup_for_memory_file(source):
+    _tu = get_tu_from_text(source, 't.cpp')
+    _proj_engine = db.ProjectEngine('test', is_in_memory = True)
+    return {'tu': _tu, 'proj_engine': _proj_engine}
+
 @nottest
 def setup_for_test_file():
     TEST_INPUT = \
@@ -95,9 +106,7 @@ int main()
 }
 """
 
-    _tu = get_tu_from_text(TEST_INPUT, 't.cpp')
-    _proj_engine = db.ProjectEngine('test', is_in_memory = True)
-    return {'tu': _tu, 'proj_engine': _proj_engine}
+    return setup_for_memory_file(TEST_INPUT)
 
 @with_param_setup(setup_for_test_file)
 def test_file_get_pending_filenames(tu, proj_engine):
@@ -159,34 +168,113 @@ def test_file_get_pending_filenames_with_multiple_files(tu, proj_engine):
     assert not pending_files
 
 
+def assert_ckind_equal(ckind, db_ckind):
+    eq_(ckind.name, db_ckind.name)
+    eq_(ckind.is_declaration(), db_ckind.is_declaration)
+    eq_(ckind.is_reference(), db_ckind.is_reference)
+    eq_(ckind.is_expression(), db_ckind.is_expression)
+    eq_(ckind.is_statement(), db_ckind.is_statement)
+    eq_(ckind.is_attribute(), db_ckind.is_attribute)
+    eq_(ckind.is_translation_unit(), db_ckind.is_translation_unit)
+    eq_(ckind.is_preprocessing(), db_ckind.is_preprocessing)
+    eq_(ckind.is_unexposed(), db_ckind.is_unexposed)
+
 @with_param_setup(setup_for_test_file)
-def test_cursor_kind(tu, proj_engine):   
-    def assert_ckind_equal(ckind, db_ckind):
-        eq_(ckind.name, db_ckind.name)
-        eq_(ckind.is_declaration(), db_ckind.is_declaration)
-        eq_(ckind.is_reference(), db_ckind.is_reference)
-        eq_(ckind.is_expression(), db_ckind.is_expression)
-        eq_(ckind.is_statement(), db_ckind.is_statement)
-        eq_(ckind.is_attribute(), db_ckind.is_attribute)
-        eq_(ckind.is_translation_unit(), db_ckind.is_translation_unit)
-        eq_(ckind.is_preprocessing(), db_ckind.is_preprocessing)
-        eq_(ckind.is_unexposed(), db_ckind.is_unexposed)
-    
+def test_cursor_kind(tu, proj_engine):
     for kind in ckind.get_all_kinds():
         db_ckind = db.CursorKind.from_clang_cursor_kind(kind, proj_engine)
         assert_ckind_equal(kind, db_ckind)
 
+
+def assert_tkind_equal(tkind, db_tkind):
+    eq_(tkind.name, db_tkind.name)
+    eq_(tkind.spelling, db_tkind.spelling)
+
 @with_param_setup(setup_for_test_file)
 def test_type_kind(tu, proj_engine):
-    def assert_tkind_equal(tkind, db_tkind):
-        eq_(tkind.name, db_tkind.name)
-        eq_(tkind.spelling, db_tkind.spelling)
-    
     for kind in _get_clang_type_kinds():
         db_tkind = db.TypeKind.from_clang_type_kind(kind, proj_engine)
         assert_tkind_equal(kind, db_tkind)
 
-@with_param_setup(setup_for_test_file)
-def test_cursor(tu, proj_engine):
+
+TEST_CURSOR_INPUT = """
+class A
+{
+    int data;
+};
+"""
+
+def assert_cursor_equal(cursor, db_cursor):
+    eq_(cursor.spelling, db_cursor.spelling)
+    eq_(cursor.displayname, db_cursor.displayname)
+    eq_(cursor.get_usr(), db_cursor.usr)
+    eq_(cursor.is_definition(), db_cursor.is_definition)
+    eq_(db_cursor.location_start, cursor.location)
+    eq_(db_cursor.location_end, cursor.extent.end)
+    assert_ckind_equal(cursor.kind, db_cursor.kind)
+
+@with_param_setup(partial(setup_for_memory_file, TEST_CURSOR_INPUT))
+def test_cursor_ctor(tu, proj_engine):
+    A_cursor = get_cursor(tu, 'A')
+    db_A_cursor = db.Cursor(A_cursor, proj_engine)
+    
+    assert_cursor_equal(A_cursor, db_A_cursor)
+
+@with_param_setup(partial(setup_for_memory_file, TEST_CURSOR_INPUT))
+def test_cursor_from_clang_cursor_with_new_cursor(tu, proj_engine):
+    # ensure that this cursor is not in DB
+    A_cursor = get_cursor(tu, 'A')
+    eq_(0, len(proj_engine.get_session().query(db.Cursor).\
+               filter(db.Cursor.usr == A_cursor.get_usr()).all()))
+    
+    db_A_cursor = db.Cursor.from_clang_cursor(A_cursor, proj_engine)
+    
+    # check if this cursor is in DB.
+    assert_cursor_equal(A_cursor, db_A_cursor)
+
+
+def fake_build_db_cursor(cursor, proj_engine):
+    
+    def build_db_cursor(cursor, parent, left):
+        db_cursor = db.Cursor(cursor, proj_engine)
+        db_cursor.parent = parent
+        db_cursor.left = left
+        
+        proj_engine.get_session().add(db_cursor)
+        
+        child_left = left + 1
+        if cursor.kind != cindex.CursorKind.TEMPLATE_TEMPLATE_PARAMETER:
+            for child in cursor.get_children():
+                child_left = build_db_cursor(child, db_cursor, child_left) + 1
+        
+        right = child_left
+        db_cursor.right = right
+    
+        proj_engine.get_session().add(db_cursor)
+        proj_engine.get_session().commit()
+        proj_engine.get_session().expire(db_cursor)
+    
+        return right
+
+    build_db_cursor(cursor, None, 0)
+
+@with_param_setup(partial(setup_for_memory_file, TEST_CURSOR_INPUT))
+def test_cursor_from_clang_cursor_with_db_cursor(tu, proj_engine):
+    A_cursor = get_cursor(tu, 'A')
+    fake_build_db_cursor(A_cursor, proj_engine)
+    # ensure that the cursor is in db
+    eq_(1, len(proj_engine.get_session().query(db.Cursor).\
+               filter(db.Cursor.usr == A_cursor.get_usr()).all()))
+    
+    db_A_cursor = db.Cursor.from_clang_cursor(A_cursor, proj_engine)
+    
+    assert_cursor_equal(A_cursor, db_A_cursor)
+
+TEST_CURSOR_TEMPLATE_INPUT = """
+
+"""
+
+@with_param_setup(partial(setup_for_memory_file, TEST_CURSOR_TEMPLATE_INPUT))
+def test_cursor_from_clang_cursor_with_tmpl_cursor(tu, proj_engine):
     pass
 
